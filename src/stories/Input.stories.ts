e927609cfc1a9c2768feb93353c3659d101cf5d2
Angel <aleangrodriguez123@gmail.com>
2023-04-19 18:44:46 -0500

    feat: add storybook for input

---
+++ src/stories/Input.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Input from '../components/Input';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: 'Input',
+  component: Input,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default: Story = {
+  args: {
+    label: 'Label',
+  },
+};
+
