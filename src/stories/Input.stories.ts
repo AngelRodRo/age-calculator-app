62b2bda08ed0e40c1d7b63a2e3c8acf63edf08fa
Angel <aleangrodriguez123@gmail.com>
2023-04-21 21:57:12 -0500

    feat: add input date styles

--- src/stories/Input.stories.ts
+++ src/stories/Input.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import Input from '../components/Input';
+import Input from '../components/DateInput';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
@@ -16,6 +16,8 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     label: 'Label',
+    name: 'name',
+    error: '',
   },
 };
 
