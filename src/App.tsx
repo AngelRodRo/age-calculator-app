45311a0464764877d21d1311566fd3c4bf22328a
Angel <aleangrodriguez123@gmail.com>
2023-04-30 11:00:08 -0500

    feat: added new components in the main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import React from 'react'
 import './App.css'
+
+import DateInput from '../src/components/DateInput'
+import ConvertButton from './components/ConvertButton'
+import DateResult from './components/DateResult'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='app'>
+      <div className='app-container'>
+        <div style={{ display: 'flex', gap: 20 }}>
+          <DateInput label='Day' />
+          <DateInput label='Month' />
+          <DateInput label='Year' />
+        </div>
+        <ConvertButton />
+
+        <DateResult type='year' date='--' />
+        <DateResult type='month' date='--' />
+        <DateResult type='day' date='--' />
+
+      </div>
     </div>
   )
 }
